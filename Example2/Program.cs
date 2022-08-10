ee046d1140807cf17f5aaf96ab931fdd5e303aaf
Luba <ponochka.85@bk.com>
2022-08-10 11:04:50 +0300

    add Example 2

---
+++ Example2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая выводит третью цифру 
+   заданного числа или сообщает, что третьей цифры нет.*/
+
+  class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите трёхзначное число или 'q' для выхода");
+            string num = Console.ReadLine();
+            if (num == "q")
+            {
+                break;
+            }
+            if (num.Length == 3)
+            { Console.WriteLine("Третья цифра числа: " + num[2]); }
+            else if (num.Length <3)
+            {
+                Console.WriteLine("Третьей цифры нет");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не трёхзначное число");
+            }
+        }
+
+    }
+}
